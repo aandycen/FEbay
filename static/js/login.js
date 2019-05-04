6f4fb9beaaa68f8046b211c73456feb5087dade6
jacmei <jackeymei0@gmail.com>
2019-05-04 04:26:01 -0400

    A lot of work was done

--- static/js/login.js
+++ static/js/login.js
@@ -5,21 +5,19 @@ var loginStatus = document.getElementById("loginStatus");
 
 loginUser = function(){
     params = { 
-	       'email': email.value,
-	       'password': password.value,
-	     };
+    	'email': email.value,
+		'password': password.value,
+	};
 
     var res = makeApiCall('/login', 'POST', params);
 
-    loginStatus.innerText = res['success']? 'Success' : res['error'];
+    loginStatus.innerText = res['error'];
 
-    if (loginStatus.innerText == 'Success'){
-	
-	res = makeApiCall('/profile', 'POST', {'email': email.value})
-	sessionStorage.setItem('user', JSON.stringify(res));
-	redirect('/');
+    if (res['success']){
+		res = makeApiCall('/profile', 'POST', {'email': email.value})
+		sessionStorage.setItem('user', JSON.stringify(res));
+		redirect('/');
     }
-    
 }
 
 loginBtn.addEventListener('click', loginUser);
